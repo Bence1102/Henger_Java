15a52a327f85767ae57a151b2d34eaf93de104d3
Bence1102 <szente.bence04@gmail.com>
2025-12-11 13:54:20 +0100

    nezet

--- src/main/HengerProgram.java
+++ src/main/HengerProgram.java
@@ -11,26 +11,35 @@ public class HengerProgram {
     private List<Henger> hengerek;
 
     public HengerProgram() {
-        this.hengerek = new ArrayList<>();
+        hengerek = new ArrayList<>();
     }
 
     public void run() {
-        lista();
-        System.out.println("Hengerek száma:" + Henger.getHengerDarab());
-        System.out.println("Átlag térfogat: " + atlagTerfogat());
-        System.out.println("Csövek összsúlya: " + csovekSulya());
+        //lista();
+        Henger h1 =  new Henger(4,8);
+        Henger th1 =  new TomorHenger(5,10);
+        Henger th2 =  new TomorHenger(5,10,0.5);
+        Henger cs1 =  new Cso(6,12,0.3);
+        Henger cs2 =  new Cso(3,6,1.5);
+        hengerek.add(h1);
+        hengerek.add(th1);
+        hengerek.add(th2);
+        hengerek.add(cs1);
+        hengerek.add(cs2);
+        int db=Henger.getHengerDarab();
+        System.out.println("Felhasznált Hengerek száma:" + db);
+        /*for(Henger h:hengerek){
+            System.out.println(h);
+        }*/
+        System.out.println("Hengerek térfogata:" + Henger.getHengerDarab());
+        System.out.println("Hengerek átlag térfogata: " + atlagTerfogat());
+        System.out.println("Csövek súlya: " + csovekSulya());
         System.out.println("\nLista:");
-        hengerek.forEach(henger -> System.out.println(henger.toString()));
-    }
-
-    public void lista() {
-        hengerek.add(new Henger(2, 10));
-        hengerek.add(new TomorHenger(2, 10, 7.8));
-        hengerek.add(new Cso(5, 20, 7.8, 1));
+        hengerek.forEach(System.out::println);
     }
 
     public double atlagTerfogat() {
-        return 1.0;
+        return 0;
     }
 
     public double csovekSulya() {
